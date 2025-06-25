2956265a75439f5465f32f78ddb40104c5c2fc1d
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-25 20:19:26 +0700

    add

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { databaseUrl } from "@/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "lib/db/drizzle",
-  schema: "lib/db/schema/index.ts",
+  out: "./lib/db/drizzle",
+  schema: "./lib/db/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: databaseUrl,
