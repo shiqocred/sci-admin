a776efba7875cd143edadd30f87c4f11cb96e691
MacBook Pro <macbookpro@sroo-punya.local>
2025-07-01 23:32:54 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "s3.sro.my.id" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "s3.sro.my.id" },
+      { protocol: "https", hostname: "s3-dev.sro.my.id" },
+    ],
   },
 };
 
