0c84ce4f641fae895214a296ec4e32e0e8e1a57f
MacBook Pro <macbookpro@sroo-punya.local>
2025-07-09 16:54:46 +0700

    add

--- src/app/api/(eccomerce)/auth/mobile/google/route.ts
+++ src/app/api/(eccomerce)/auth/mobile/google/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: NextRequest) {
           name,
           image,
           emailVerified: new Date(),
-          role: null,
+          role: "BASIC",
         })
         .returning({ id: users.id });
 
