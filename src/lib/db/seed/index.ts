222381d123c88c6d8558bb4eb901674b8d68d134
MacBook Pro <macbookpro@sroo-punya.local>
2025-08-29 20:00:38 +0700

    add

--- src/lib/db/seed/index.ts
+++ src/lib/db/seed/index.ts
@@ -1,4 +1,4 @@
 export * from "./user";
-// export * from "./store-detail";
-// export * from "./couriers";
-// export * from "./pets";
+export * from "./store-detail";
+export * from "./couriers";
+export * from "./pets";
